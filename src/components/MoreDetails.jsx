988af05221619792ef6673b9060977a09640e1e9
Viacheslav <102405139+vch-sh@users.noreply.github.com>
2023-07-27 12:13:17 +0300

    Optimized forming objects with data for each day that are passing to Tabs.jsx.

--- src/components/MoreDetails.jsx
+++ src/components/MoreDetails.jsx
@@ -19,81 +19,29 @@ const MoreDetails = ({ hourly }) => {
 	const detailedRainData = getDataForWeekBySpecificHour(hourly.hourly.rain);
 	const detailedSnowfallData = getDataForWeekBySpecificHour(hourly.hourly.snowfall);
 	
-	const getDataForSpecificDay = (dataForEntireWeekArray, startPosition, endPosition) => {
-		const copy = dataForEntireWeekArray.slice(startPosition, endPosition);
-		return copy;
+	const getDataForSpecificDay = (dataForEntireWeekArray, startIndex, endIndex) => {
+		return dataForEntireWeekArray.slice(startIndex, endIndex);
 	}
 
-	// Optimize this code
-	const tempForSpecificDay = {
-		0: getDataForSpecificDay(detailedTemperatureData, 0, 8),
-		1: getDataForSpecificDay(detailedTemperatureData, 8, 16),
-		2: getDataForSpecificDay(detailedTemperatureData, 16, 24),
-		3: getDataForSpecificDay(detailedTemperatureData, 24, 32),
-		4: getDataForSpecificDay(detailedTemperatureData, 32, 40),
-		5: getDataForSpecificDay(detailedTemperatureData, 40, 48),
-		6: getDataForSpecificDay(detailedTemperatureData, 48, 56),
+	const formObjectDataForSpeciticDay = (detailedData) => {
+		const dataObj = {};
+
+		for (let i = 0; i < 7; i++) {
+			const startIndex = i * 8;
+			const endIndex = startIndex + 8;
+			dataObj[i] = getDataForSpecificDay(detailedData, startIndex, endIndex);
+		}
+
+		return dataObj;
 	}
 
-	const humidityForSpecificDay = {
-		0: getDataForSpecificDay(detailedHumidityData, 0, 8),
-		1: getDataForSpecificDay(detailedHumidityData, 8, 16),
-		2: getDataForSpecificDay(detailedHumidityData, 16, 24),
-		3: getDataForSpecificDay(detailedHumidityData, 24, 32),
-		4: getDataForSpecificDay(detailedHumidityData, 32, 40),
-		5: getDataForSpecificDay(detailedHumidityData, 40, 48),
-		6: getDataForSpecificDay(detailedHumidityData, 48, 56),
-	}
-
-	const pressureForSpecificDay = {
-		0: getDataForSpecificDay(detailedPressureData, 0, 8),
-		1: getDataForSpecificDay(detailedPressureData, 8, 16),
-		2: getDataForSpecificDay(detailedPressureData, 16, 24),
-		3: getDataForSpecificDay(detailedPressureData, 24, 32),
-		4: getDataForSpecificDay(detailedPressureData, 32, 40),
-		5: getDataForSpecificDay(detailedPressureData, 40, 48),
-		6: getDataForSpecificDay(detailedPressureData, 48, 56),
-	}
-
-	const cloudcoverForSpecificDay = {
-		0: getDataForSpecificDay(detailedCloudcoverData, 0, 8),
-		1: getDataForSpecificDay(detailedCloudcoverData, 8, 16),
-		2: getDataForSpecificDay(detailedCloudcoverData, 16, 24),
-		3: getDataForSpecificDay(detailedCloudcoverData, 24, 32),
-		4: getDataForSpecificDay(detailedCloudcoverData, 32, 40),
-		5: getDataForSpecificDay(detailedCloudcoverData, 40, 48),
-		6: getDataForSpecificDay(detailedCloudcoverData, 48, 56),
-	}
-
-	const windspeedForSpecificDay = {
-		0: getDataForSpecificDay(detailedWindSpeedData, 0, 8),
-		1: getDataForSpecificDay(detailedWindSpeedData, 8, 16),
-		2: getDataForSpecificDay(detailedWindSpeedData, 16, 24),
-		3: getDataForSpecificDay(detailedWindSpeedData, 24, 32),
-		4: getDataForSpecificDay(detailedWindSpeedData, 32, 40),
-		5: getDataForSpecificDay(detailedWindSpeedData, 40, 48),
-		6: getDataForSpecificDay(detailedWindSpeedData, 48, 56),
-	}
-	
-	const rainForSpecificDay = {
-		0: getDataForSpecificDay(detailedRainData, 0, 8),
-		1: getDataForSpecificDay(detailedRainData, 8, 16),
-		2: getDataForSpecificDay(detailedRainData, 16, 24),
-		3: getDataForSpecificDay(detailedRainData, 24, 32),
-		4: getDataForSpecificDay(detailedRainData, 32, 40),
-		5: getDataForSpecificDay(detailedRainData, 40, 48),
-		6: getDataForSpecificDay(detailedRainData, 48, 56),
-	}
-
-	const snowfallForSpecificDay = {
-		0: getDataForSpecificDay(detailedSnowfallData, 0, 8),
-		1: getDataForSpecificDay(detailedSnowfallData, 8, 16),
-		2: getDataForSpecificDay(detailedSnowfallData, 16, 24),
-		3: getDataForSpecificDay(detailedSnowfallData, 24, 32),
-		4: getDataForSpecificDay(detailedSnowfallData, 32, 40),
-		5: getDataForSpecificDay(detailedSnowfallData, 40, 48),
-		6: getDataForSpecificDay(detailedSnowfallData, 48, 56),
-	}
+	const tempForSpecificDay = formObjectDataForSpeciticDay(detailedTemperatureData);
+	const humidityForSpecificDay = formObjectDataForSpeciticDay(detailedHumidityData);
+	const pressureForSpecificDay = formObjectDataForSpeciticDay(detailedPressureData);
+	const cloudcoverForSpecificDay = formObjectDataForSpeciticDay(detailedCloudcoverData);
+	const windspeedForSpecificDay = formObjectDataForSpeciticDay(detailedWindSpeedData);
+	const rainForSpecificDay = formObjectDataForSpeciticDay(detailedRainData);
+	const snowfallForSpecificDay = formObjectDataForSpeciticDay(detailedSnowfallData);
 
 	return (
 		<div className={styles.moreDetails}>
